296ec0106b4eaa1e0f5e0f7edc8d9de1f48a30b3
tudai1101 <127816770+tudai1101@users.noreply.github.com>
2023-12-11 09:08:12 +0700

    detect cohesion in controller package

--- AIMS/src/controller/BaseController.java
+++ AIMS/src/controller/BaseController.java
@@ -10,6 +10,7 @@ import entity.media.Media;
  * This class is the base controller for our AIMS project
  * @author nguyenlm
  */
+// Functional cohesion 
 public class BaseController {
     
     /**
